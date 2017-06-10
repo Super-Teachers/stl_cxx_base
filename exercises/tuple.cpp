87c69f9f996b8b738a22f67023b8129ae25ac6d3
Bartosz Taczała <bartosz.taczala@mobica.com>
2017-06-10 09:37:11 +0200

    Tuple first iteration

--- exercises/tuple.cpp
+++ exercises/tuple.cpp
@@ -1,33 +1,58 @@
 
 #include <gtest/gtest.h>
+#include <numeric>
 #include <utility>
 
 struct Accumulator {
-    void add(const std::string &, int, double) {
-        // TODO: Implement me
+    typedef std::tuple<std::string, int, double> MyTuple;
+
+    void add(const std::string &s, int i, double d) {
+        auto t = std::make_tuple(s, i, d);
+        _vector.push_back(t);
     }
 
-    std::string sum() {
-        // TODO: Implement me
-        return "";
+    MyTuple sum() const {
+        std::tuple<std::string, int, double> temp;
+        std::get<0>(temp) = std::get<0>(_vector[0]);
+
+        temp = std::accumulate(
+            _vector.begin() + 1, _vector.end(), temp,
+            [](auto tup, auto tup2) -> MyTuple {
+                return std::make_tuple(
+                    std::get<0>(tup) + "+" + std::get<0>(tup2), 0, 0.0f);
+            });
+
+        // first version
+        // for (const auto i : _vector) {
+        // if (std::get<0>(temp).empty()) {
+        // std::get<0>(temp) = std::get<0>(i);
+        //} else {
+        // std::get<0>(temp) = std::get<0>(temp) + "+" + std::get<0>(i);
+        //}
+
+        // std::get<1>(temp) = std::get<1>(temp) + std::get<1>(i);
+        // std::get<2>(temp) = std::get<2>(temp) + std::get<2>(i);
+        //}
+
+        return temp;
     }
 
    private:
-    // std::vector<std::tuple<...>> _vector;
+    std::vector<MyTuple> _vector;
 };
 
 TEST(Accumulator, one) {
     Accumulator acc;
     acc.add("test", 10, 0.4f);
-    // EXPECT_EQ(acc.sum(), std::make_tuple("test", 10, 0.4));
-    EXPECT_FALSE(true);
+    auto result = std::make_tuple("test", 10, 0.4);
+    EXPECT_EQ(std::get<0>(acc.sum()), std::get<0>(result));
 }
 TEST(Accumulator, two) {
     Accumulator acc;
     acc.add("test", 10, 0.4f);
     acc.add("test", 10, 0.4f);
-    // EXPECT_EQ(acc.sum(), std::make_tuple("test+test", 20, 0.8));
-    EXPECT_FALSE(true);
+    auto result = std::make_tuple("test+test", 10, 0.4);
+    EXPECT_EQ(std::get<0>(acc.sum()), std::get<0>(result));
 }
 
 int main(int argc, char **argv) {
