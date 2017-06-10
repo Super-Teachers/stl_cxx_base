3ac6bc859886512eec03cc72df09ce7f6c147b51
Bartosz Taczała <bartosz.taczala@mobica.com>
2017-06-10 08:53:33 +0200

    Pair

--- exercises/pair.cpp
+++ exercises/pair.cpp
@@ -1,25 +1,32 @@
 #include <gtest/gtest.h>
 #include <utility>
 
-/*
 namespace my_std {
-?? my_std::atoi(??)
+std::pair<int, bool> atoi(const std::string& a) {
+    auto val = std::atoi(a.c_str());
+
+    if (val == 0 && a == "0") return std::make_pair(val, true);
+    return std::make_pair(val, val == 0 ? false : true);
 }
- */
+}
 
 TEST(atoi, incorrect_atoi) {
-    //auto result = my_std::atoi("not a number");
-    //EXPECT_EQ(this has to be a bad value);
-    EXPECT_TRUE(false);
+    auto result = my_std::atoi("not a number");
+    EXPECT_FALSE(result.second);
 }
 
 TEST(atoi, correct_atoi) {
-    //std::pair<int, bool> result = my_std::atoi("10");
-    // this has to be a good value
-    EXPECT_TRUE(false);
+    std::pair<int, bool> result = my_std::atoi("10");
+    EXPECT_TRUE(result.second);
+    EXPECT_EQ(result.first, 10);
 }
 
-int main(int argc, char **argv) {
+TEST(atoi, zero) {
+    std::pair<int, bool> result = my_std::atoi("0");
+    EXPECT_TRUE(result.second);
+    EXPECT_EQ(result.first, 0);
+}
+int main(int argc, char** argv) {
     ::testing::InitGoogleTest(&argc, argv);
     return RUN_ALL_TESTS();
 }
