195295c98b03185b82fab4de64f34ed02bc292da
Bartosz Taczała <bartosz.taczala@mobica.com>
2017-06-10 09:55:09 +0200

    Tuple - iteration 2

--- exercises/tuple.cpp
+++ exercises/tuple.cpp
@@ -7,44 +7,23 @@ struct Accumulator {
     typedef std::tuple<std::string, int, double> MyTuple;
 
     void add(const std::string &s, int i, double d) {
-        auto t = std::make_tuple(s, i, d);
-        _vector.push_back(t);
+        std::get<0>(_result) = std::get<0>(_result) + "+" + s;
+        std::get<1>(_result) = std::get<1>(_result) + i;
+        std::get<2>(_result) = std::get<2>(_result) + i;
     }
 
-    MyTuple sum() const {
-        std::tuple<std::string, int, double> temp;
-        std::get<0>(temp) = std::get<0>(_vector[0]);
-
-        temp = std::accumulate(
-            _vector.begin() + 1, _vector.end(), temp,
-            [](auto tup, auto tup2) -> MyTuple {
-                return std::make_tuple(
-                    std::get<0>(tup) + "+" + std::get<0>(tup2), 0, 0.0f);
-            });
-
-        // first version
-        // for (const auto i : _vector) {
-        // if (std::get<0>(temp).empty()) {
-        // std::get<0>(temp) = std::get<0>(i);
-        //} else {
-        // std::get<0>(temp) = std::get<0>(temp) + "+" + std::get<0>(i);
-        //}
-
-        // std::get<1>(temp) = std::get<1>(temp) + std::get<1>(i);
-        // std::get<2>(temp) = std::get<2>(temp) + std::get<2>(i);
-        //}
-
-        return temp;
+    auto sum() const {
+        return _result;
     }
 
    private:
-    std::vector<MyTuple> _vector;
+    MyTuple _result;
 };
 
 TEST(Accumulator, one) {
     Accumulator acc;
     acc.add("test", 10, 0.4f);
-    auto result = std::make_tuple("test", 10, 0.4);
+    Accumulator::MyTuple result = std::make_tuple("test", 10, 0.4);
     EXPECT_EQ(std::get<0>(acc.sum()), std::get<0>(result));
 }
 TEST(Accumulator, two) {
