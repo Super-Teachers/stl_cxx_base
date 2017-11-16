546ea48af1e178163760b82b6e52a05391cc7c29
Bartek Taczała <bartek.taczala@gmail.com>
2017-11-16 01:07:09 +0100

    clang-format applied

--- exercises/vector_vs_list.cpp
+++ exercises/vector_vs_list.cpp
@@ -1,52 +1,49 @@
-#include <iostream>
-#include <vector>
-#include <list>
+#include <benchmark/benchmark.h>
 #include <array>
 #include <complex>
+#include <iostream>
+#include <list>
 #include <numeric>
 #include <random>
-#include <benchmark/benchmark.h>
+#include <vector>
 
-
-template<typename IntType> 
-IntType randomValue()
-{
+template <typename IntType>
+IntType randomValue() {
     return {100};
 }
 
 struct BigHeapData {
-    BigHeapData(double val )
-    {
+    BigHeapData(double val) {
         complexs.reserve(1024);
         std::fill_n(complexs.begin(), 1024, std::complex<double>(val, val));
     }
     std::vector<std::complex<double>> complexs;
-
 };
 
-template<typename T> 
+template <typename T>
 using is_vector = std::is_same<std::vector<typename T::value_type>, T>;
-template<typename Container> void BM_push_backs(benchmark::State& state)
-{
+template <typename Container>
+void BM_push_backs(benchmark::State& state) {
     Container ctx;
     if constexpr (is_vector<Container>::value) {
         ctx.reserve(state.range(0));
     }
 
-    while(state.KeepRunning()) {
-        for (int i = state.range(0); i--; ) {
+    while (state.KeepRunning()) {
+        for (int i = state.range(0); i--;) {
             ctx.push_back(randomValue<typename Container::value_type>());
         }
     }
 }
 
-template<typename Container> void BM_find_value(benchmark::State& state)
-{
+template <typename Container>
+void BM_find_value(benchmark::State& state) {
     static std::vector<typename Container::value_type> container;
     if (container.empty()) {
         container.reserve(state.range());
         std::iota(container.begin(), container.end(), 0);
-        std::shuffle(container.begin(), container.end(),  std::mt19937{std::random_device{}()});
+        std::shuffle(container.begin(), container.end(),
+                     std::mt19937{std::random_device{}()});
     }
 
     static typename Container::value_type randomValue = 500;
@@ -55,14 +52,20 @@ template<typename Container> void BM_find_value(benchmark::State& state)
 auto startRange = 8;
 auto stopRange = 8 << 16;
 
-BENCHMARK_TEMPLATE(BM_push_backs, std::vector<int>)->Range(startRange, stopRange);
-BENCHMARK_TEMPLATE(BM_push_backs, std::vector<float>)->Range(startRange, stopRange);
-BENCHMARK_TEMPLATE(BM_push_backs, std::vector<std::array<char, 1024>>)->Range(startRange, stopRange);
-BENCHMARK_TEMPLATE(BM_push_backs, std::vector<BigHeapData>)->Range(startRange, stopRange);
+BENCHMARK_TEMPLATE(BM_push_backs, std::vector<int>)
+    ->Range(startRange, stopRange);
+BENCHMARK_TEMPLATE(BM_push_backs, std::vector<float>)
+    ->Range(startRange, stopRange);
+BENCHMARK_TEMPLATE(BM_push_backs, std::vector<std::array<char, 1024>>)
+    ->Range(startRange, stopRange);
+BENCHMARK_TEMPLATE(BM_push_backs, std::vector<BigHeapData>)
+    ->Range(startRange, stopRange);
 BENCHMARK_TEMPLATE(BM_push_backs, std::list<int>)->Range(startRange, stopRange);
-BENCHMARK_TEMPLATE(BM_push_backs, std::list<float>)->Range(startRange, stopRange);
-BENCHMARK_TEMPLATE(BM_push_backs, std::list<std::array<char, 1024>>)->Range(startRange, stopRange);
-BENCHMARK_TEMPLATE(BM_push_backs, std::list<BigHeapData>)->Range(startRange, stopRange);
+BENCHMARK_TEMPLATE(BM_push_backs, std::list<float>)
+    ->Range(startRange, stopRange);
+BENCHMARK_TEMPLATE(BM_push_backs, std::list<std::array<char, 1024>>)
+    ->Range(startRange, stopRange);
+BENCHMARK_TEMPLATE(BM_push_backs, std::list<BigHeapData>)
+    ->Range(startRange, stopRange);
 
 BENCHMARK_MAIN();
-
