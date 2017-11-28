a50bf235549b1d27bef1354ea6357357947c70ad
Bartosz Taczała <bartosz.taczala@mobica.com>
2017-11-28 12:25:28 +0100

    Squashed commit of the following:
    Cookbook template finished

--- exercises/cookbook.cpp
+++ exercises/cookbook.cpp
@@ -5,76 +5,35 @@
 #include <unordered_map>
 #include <vector>
 
-using keywords = std::set<std::string>;
-struct Ingredient {
-    std::string name;
-    enum class Type { Litre = 0, Gram } type;
-    double quantity;
-};
+#include "cookbook.hpp"
 
-bool operator==(const Ingredient& lhs, const Ingredient& rhs) {
-    return lhs.name == rhs.name;
+template <typename T>
+cooking::cost_t cost(cooking::recipeList&) {}
+
+TEST(Cookbook, add_recipe) {
+    /*
+      Cookbook cb;
+      Recipe budyn { "Budyn "};
+      budyn.ingreds = { {"mleko", "50" }, {"masło", "100"} };
+      cb.addRecipe(budyn);
+      ASSERT_EQ(cb.findRecipes("Budyn").size(), 1);
+      EXPECT_EQ(cost(cb.findRecipes("Budyn")), 150 );
+       */
 }
 
-struct Recipe {
-    using Ingredients_t = std::vector<Ingredient>;
-    std::string title;
-    Ingredients_t ingredients;
-    keywords keyz;
-};
+TEST(Cookbook, different_version_of_recipe) {
+    /*
+      Cookbook cb;
+      Recipe budyn { "Budyn "};
+      budyn.ingreds = { {"mleko", "50" }, {"masło", "100"} };
+      cb.addRecipe(budyn);
 
-bool operator==(const Recipe& lhs, const Recipe& rhs) {
-    return lhs.title == rhs.title && lhs.ingredients == rhs.ingredients;
-}
+      budyn.ingreds.push_back( {"czekolada", "100"} );
+      cb.addRecipe(budyn, {"czekolada"});
 
-struct Cookbook {
-    using Recipes_t = std::list<Recipe>;
-    using RecipePtr_t = Recipes_t::iterator;
-    using MultiMap_t = std::unordered_multimap<std::string, RecipePtr_t>;
-    void addRecipe(const Recipe& recipe) {
-        auto it = std::find(_data.begin(), _data.end(), recipe);
-        if (it != _data.end()) {
-        } else {
-            _data.push_back({recipe});
-            auto it = _data.end();
-            _titles.insert(MultiMap_t::value_type(recipe.title, --it));
-        }
-    }
-
-    std::vector<Recipe> find(const std::string& title) {
-        auto its = _titles.equal_range(title);
-        std::vector<Recipe> ret;
-        ret.reserve(std::distance(its.first, its.second));
-
-        std::transform(
-            its.first, its.second, std::back_inserter(ret),
-            [](const MultiMap_t::value_type& it) { return *it.second; });
-        return ret;
-    }
-
-    Recipes_t find(const keywords& keyz) { return Recipes_t{}; }
-
-    std::size_t size() const noexcept { return _data.size(); }
-
-   private:
-    Recipes_t _data;
-
-    // titles
-    std::unordered_multimap<std::string, RecipePtr_t> _titles;
-};
-
-TEST(Cookbook, insert_recipe) {
-    Cookbook cb;
-    cb.addRecipe(
-        Recipe{"cake", {Ingredient{"butter", Ingredient::Type::Gram, 0.4}}});
-
-    EXPECT_EQ(cb.size(), 1);
-
-    cb.addRecipe(
-        Recipe{"cake", {Ingredient{"sugar", Ingredient::Type::Gram, 0.4}}});
-
-    EXPECT_EQ(cb.size(), 2);
-    EXPECT_EQ(cb.find("cake").size(), 2);
+      EXPECT_EQ(cb.findRecipe("budyn").size(), 2);
+      EXPECT_EQ(cb.findByKeywords("czekolada").size(), 1);
+       */
 }
 
 int main(int argc, char** argv) {
