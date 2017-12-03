a7729d87b8eed6c31759f54a05d1a072e2fd1371
Bartosz Taczała <bartosz.taczala@mobica.com>
2017-12-03 11:56:34 +0100

    cookbook impl

--- exercises/cookbook.cpp
+++ exercises/cookbook.cpp
@@ -1,39 +1,53 @@
 #include <gtest/gtest.h>
 #include <list>
 #include <map>
+#include <numeric>
 #include <string>
 #include <unordered_map>
 #include <vector>
 
 #include "cookbook.hpp"
 
-template <typename T>
-cooking::cost_t cost(cooking::recipeList&) {}
+double cost(const cooking::RecipeList& recipes) {
+    auto value = std::accumulate(
+        recipes.begin(), recipes.end(), 0.0,
+        [](double init, const auto& recipe) -> double {
+            return init + std::accumulate(recipe._ingreds.begin(),
+                                          recipe._ingreds.end(), 0.0,
+                                          [](double innerInit, const auto& i) {
+                                              return innerInit + i._quantity;
+                                          });
+        });
+
+    return value;
+}
 
 TEST(Cookbook, add_recipe) {
-    /*
-      Cookbook cb;
-      Recipe budyn { "Budyn "};
-      budyn.ingreds = { {"mleko", "50" }, {"masło", "100"} };
-      cb.addRecipe(budyn);
-      ASSERT_EQ(cb.findRecipes("Budyn").size(), 1);
-      EXPECT_EQ(cost(cb.findRecipes("Budyn")), 150 );
-       */
+    using cooking::Recipe;
+    using cooking::v1::Cookbook;
+    Cookbook cb;
+    Recipe budyn{"Budyn"};
+    budyn._ingreds = {{"mleko", 50}, {"masło", 100}};
+    cb.addRecipe(budyn, {"deser"});
+    ASSERT_EQ(cb.findRecipes("Budyn").size(), 1);
+    EXPECT_EQ(cost(cb.findRecipes("Budyn")), 150);
+
+    EXPECT_EQ(cb.findByKeywords({"deser"}).size(), 1);
 }
 
 TEST(Cookbook, different_version_of_recipe) {
-    /*
-      Cookbook cb;
-      Recipe budyn { "Budyn "};
-      budyn.ingreds = { {"mleko", "50" }, {"masło", "100"} };
-      cb.addRecipe(budyn);
+    using cooking::Recipe;
+    using cooking::v1::Cookbook;
+    Cookbook cb;
+    Recipe budyn{"Budyn"};
+    budyn._ingreds = {{"mleko", 50}, {"masło", 100}};
+    cb.addRecipe(budyn);
 
-      budyn.ingreds.push_back( {"czekolada", "100"} );
-      cb.addRecipe(budyn, {"czekolada"});
+    budyn._ingreds.push_back({"czekolada", 100});
+    cb.addRecipe(budyn, {"czekolada"});
 
-      EXPECT_EQ(cb.findRecipe("budyn").size(), 2);
-      EXPECT_EQ(cb.findByKeywords("czekolada").size(), 1);
-       */
+    EXPECT_EQ(cb.findRecipes("Budyn").size(), 2);
+    EXPECT_EQ(cb.findByKeywords({"czekolada"}).size(), 1);
 }
 
 int main(int argc, char** argv) {
--- exercises/cookbook.hpp
+++ exercises/cookbook.hpp
@@ -1,40 +1,75 @@
 #ifndef COOKBOOK_HPP_QBXUGV7S
 #define COOKBOOK_HPP_QBXUGV7S
 
+#include <map>
+#include <string>
+#include <vector>
+
 namespace cooking {
 
-typedef double title;
-typedef double string;
-typedef float recipeList;
-
-typedef float keywords;
-typedef double cost_t;
+struct Ingredient;
+struct Recipe;
+using Ingredients = std::vector<Ingredient>;
+using RecipeList = std::vector<Recipe>;
+using Keywords = std::vector<std::string>;
 
 struct Ingredient {
-    string name;
-    double quantity;
+    std::string _name;
+    double _quantity;
 };
-
-typedef float ingredients_t;
 
 struct Recipe {
-    ingredients_t ingreds;
-    cost_t cost() const;
-
+    std::string _title;
+    Ingredients _ingreds;
+    std::string _text;
 };
 
+namespace v1 {
 struct Cookbook {
-    void addRecipe(Recipe, keywords = keywords{});
+    void addRecipe(const Recipe& r, const Keywords& keywords = Keywords{}) {
+        auto it = _data.insert(std::make_pair(r._title, r));
+
+        if (!keywords.empty()) {
+            _keywords.insert(std::make_pair(keywords.at(0), it));
+        }
+    }
+
     //! Fast O(logN) lookup
-    recipeList findByKeywords(keywords);
+    RecipeList findByKeywords(const Keywords& k) {
+        auto its = _keywords.equal_range(k.at(0));
+        RecipeList ret;
+        auto it = its.first;
+        while (it != its.second) {
+            ret.push_back(it->second->second);
+            std::advance(it, 1);
+        }
+        return ret;
+    }
 
     //! Search all recipes that can be created with a given recipes
-    recipeList search(ingredients_t ingreds);
+    RecipeList search(const Ingredients& ingreds) { return RecipeList{}; }
 
     //! fast O(logN) lookup
-    recipeList findRecipes(title);
+    RecipeList findRecipes(const std::string& title) {
+        auto its = _data.equal_range(title);
+        RecipeList ret;
+        auto it = its.first;
+        while (it != its.second) {
+            ret.push_back(it->second);
+            std::advance(it, 1);
+        }
+        return ret;
+    }
+
+   private:
+    //                              title,      Recipe
+    using Recipes = std::multimap<std::string, Recipe>;
+    Recipes _data;
+    //               keyword    Recipe
+    std::multimap<std::string, Recipes::const_iterator> _keywords;
 };
 
+}  // namespace v1
 }  // namespace cooking
 
 #endif /* end of include guard: COOKBOOK_HPP_QBXUGV7S */
