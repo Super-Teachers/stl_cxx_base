76bdb5cd0722e858e87959cab698e75283819b16
Bartosz Taczała <bartosz.taczala@mobica.com>
2018-12-01 10:56:56 +0100

    czwarte cwiczenie

--- exercises/main.cpp
+++ exercises/main.cpp
@@ -4,28 +4,8 @@
 #include <list>
 #include <vector>
 
-std::string toUpper(const std::string& s) {
-    std::string tmp = s;
-    std::for_each(tmp.begin(), tmp.end(),
-                  [](char& chr) { chr = ::toupper(static_cast<int>(chr)); });
-
-    return tmp;
-}
-std::string toUpper2(const std::string& s) {
-    std::string tmp;
-    std::for_each(s.begin(), s.end(),
-                  [&tmp](char chr) { tmp += ::tolower(chr); });
-
-    return tmp;
+std::size_t odds(const std::vector<int>& v) {
+    return std::count_if(v.begin(), v.end(), [](int v) { return v % 2 != 0; });
 }
 
-std::string toUpper3(const std::string& s) {
-    std::string tmp;
-    auto predicate = [](char c) { return ::tolower(c); };
-    std::transform(s.begin(), s.end(), std::back_inserter(tmp), predicate);
-    return tmp;
-}
-
-int main(int argc, char** argv) {
-    std::cout << toUpper3(std::string{"aa"}) << "\n";
-}
+int main(int argc, char** argv) { std::cout << odds({1, 2, 3, 4, 5}) << "\n"; }
