629977588d4dc6cf64c9e3fbad8d45a5943f8f87
Bartosz Taczała <bartosz.taczala@mobica.com>
2018-12-01 10:25:03 +0100

    drugie cwiczenie

--- exercises/main.cpp
+++ exercises/main.cpp
@@ -4,42 +4,21 @@
 #include <list>
 #include <vector>
 
-void print(const std::vector<int>& v) {
-    std::for_each(v.begin(), v.end(), [](int val) { std::cout << val; });
-    std::cout << "\n";
+std::string toUpper(const std::string& s) {
+    std::string tmp = s;
+    std::for_each(tmp.begin(), tmp.end(),
+                  [](char& chr) { chr = ::toupper(static_cast<int>(chr)); });
+
+    return tmp;
 }
-void print2(const std::vector<int>& v) {
-    std::cout << "[";
-    std::for_each(v.begin(), v.end() - 1, [](int v) { std::cout << v << ","; });
-    std::cout << *(v.end() - 1);
-    std::cout << "]";
-    std::cout << "\n";
-}
+std::string toUpper2(const std::string& s) {
+    std::string tmp;
+    std::for_each(s.begin(), s.end(),
+                  [&tmp](char chr) { tmp += ::tolower(chr); });
 
-template <typename T>
-void print3(T&& t, std::ostream& os = std::cout) {
-    if (std::begin(t) == std::end(t)) {
-        std::cout << "[]";
-        return;
-    }
-
-    std::cout << "[";
-
-    auto en = std::end(t);
-    std::advance(en, -1);
-    std::for_each(std::begin(t), en, [](int v) { std::cout << v << ","; });
-    std::cout << *(en);
-    std::cout << "]";
-    std::cout << "\n";
+    return tmp;
 }
 
 int main(int argc, char** argv) {
-    std::vector<int> v{1, 2, 3, 4, 5};
-    print(v);
-    print2(v);
-
-    int arr[] = {1, 2, 3};
-
-    print3(arr, std::cout);
-    print3(std::list<int>{});
+    std::cout << toUpper(std::string{"aa"}) << "\n";
 }
