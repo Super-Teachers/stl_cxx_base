d67f555222a289c8fea37da67addb2466fa05412
Bartosz Taczała <bartosz.taczala@mobica.com>
2018-12-02 10:58:27 +0100

    transform

--- exercises/main.cpp
+++ exercises/main.cpp
@@ -1,5 +1,6 @@
 #include <gtest/gtest.h>
 #include <algorithm>
+#include <cmath>
 #include <iterator>
 #include <list>
 #include <vector>
@@ -15,15 +16,20 @@ void print(T&& t, std::ostream& os = std::cout) {
 }
 
 template <typename T1, typename T2>
-void copy(T1&& t1, T2&& t2) {
-    std::copy_if(std::begin(t1), std::end(t1), std::back_inserter(t2),
-                 [](auto v) { return v % 2 == 0; });
+std::vector<typename T1::value_type> convert(T1&& t1, T2&& t2) {
+    std::vector<typename T1::value_type> asd;
+
+    std::transform(std::begin(t1), std::end(t1), std::begin(t2),
+                   std::back_inserter(asd),
+                   [](auto n, auto k) {
+                       return std::pow(n, k);
+                   });
+
+    return asd;
 }
 
 int main(int argc, char** argv) {
-    std::vector<int> one{1, 2, 3};
-    std::vector<int> two;
-
-    copy(one, two);
-    print(two);
+    const auto strs =
+        convert(std::vector<int>{1, 2, 3}, std::list<int>{1, 2, 3});
+    print(strs);
 }
