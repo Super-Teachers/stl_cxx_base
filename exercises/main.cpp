97c39d3dcd573b6741d0e6828cfd2b43edbecc01
Bartosz Taczała <bartosz.taczala@mobica.com>
2018-12-02 10:17:09 +0100

    std::equal

--- exercises/main.cpp
+++ exercises/main.cpp
@@ -4,15 +4,34 @@
 #include <list>
 #include <vector>
 
-float closest(const std::vector<float>& v) {
-    auto it =
-        std::min_element(v.begin(), v.end(), [](auto dzidek, auto babcia) {
-            return std::abs(current) < std::abs(smallest);
-        });
+#include <benchmark/benchmark.h>
 
-    return *it;
+template <typename T1, typename T2>
+bool areSame(T1&& t1, T2&& t2) {
+    // return std::equal(std::begin(t1), std::end(t1), std::begin(t2));
+    //
+    // p = std::pair < iterator, iterator>
+    //const std::pair<typename T1::iterator, typename T2::iterator> p =
+        //std::mismatch(std::begin(t1), std::end(t1), std::begin(t2));
+    const auto p =
+        std::mismatch(std::begin(t1), std::end(t1), std::begin(t2));
+    return p.first == std::end(t1) && p.second == std::end(t2);
+
+    typename T1::iterator it;
 }
 
+bool isPalindrome ( const std::string& s)
+{
+}
+
+
+BENCHMARK()
+
 int main(int argc, char** argv) {
-    std::cout << closest(std::vector<float>{-1.f, -0.9, 1.f}) << "\n";
+    std::cout << std::boolalpha
+              << areSame(std::vector<int>{1, 2, 3}, std::list<int>{1, 2, 3})
+              << "\n";
+    std::cout << std::boolalpha
+              << areSame(std::vector<int>{1, 2, 4}, std::list<int>{1, 2, 3})
+              << "\n";
 }
