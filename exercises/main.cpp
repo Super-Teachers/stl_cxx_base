7be2f086f764c841d6785919deb8ce5c11589de8
Bartosz Taczała <bartosz.taczala@mobica.com>
2018-12-02 10:44:34 +0100

    copy_if

--- exercises/main.cpp
+++ exercises/main.cpp
@@ -4,11 +4,26 @@
 #include <list>
 #include <vector>
 
-bool isPalindrom(const std::string& s) {
-    return std::equal(s.begin(), s.begin() + s.size() / 2, s.rbegin());
+template <typename T>
+void print(T&& t, std::ostream& os = std::cout) {
+    std::cout << "[";
+    std::for_each(std::begin(t), std::end(t) - 1,
+                  [&os](auto v) { os << v << ", "; });
+
+    std::cout << *(std::end(t) - 1);
+    std::cout << "]\n";
+}
+
+template <typename T1, typename T2>
+void copy(T1&& t1, T2&& t2) {
+    std::copy_if(std::begin(t1), std::end(t1), std::back_inserter(t2),
+                 [](auto v) { return v % 2 == 0; });
 }
 
 int main(int argc, char** argv) {
-    std::cout << std::boolalpha << isPalindrom("kajak") << "\n";
-    std::cout << std::boolalpha << isPalindrom("zlo") << "\n";
+    std::vector<int> one{1, 2, 3};
+    std::vector<int> two;
+
+    copy(one, two);
+    print(two);
 }
