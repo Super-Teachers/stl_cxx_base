6934bc1a1e4fd10b9b4ae12bf1d4661d60b2928c
Bartosz Taczała <bartosz.taczala@mobica.com>
2018-12-02 11:47:45 +0100

    all lowercase

--- exercises/main.cpp
+++ exercises/main.cpp
@@ -15,21 +15,18 @@ void print(T&& t, std::ostream& os = std::cout) {
     std::cout << "]\n";
 }
 
-template <typename T1, typename T2>
-std::vector<typename T1::value_type> convert(T1&& t1, T2&& t2) {
-    std::vector<typename T1::value_type> asd;
+bool isAllLowercase(const std::string& s) {
+    return std::all_of(s.begin(), s.end(),
+                       [](char c) { return c == ::tolower(c); });
+}
 
-    std::transform(std::begin(t1), std::end(t1), std::begin(t2),
-                   std::back_inserter(asd),
-                   [](auto n, auto k) {
-                       return std::pow(n, k);
-                   });
-
-    return asd;
+void replaceAll(std::vector<std::string>& strs) {
+    std::replace_if(strs.begin(), strs.end(), isAllLowercase,
+                    std::string{"bummer"});
 }
 
 int main(int argc, char** argv) {
-    const auto strs =
-        convert(std::vector<int>{1, 2, 3}, std::list<int>{1, 2, 3});
-    print(strs);
+    std::vector<std::string> v{"Ala", "bob", "AA"};
+    replaceAll(v);
+    print(v);
 }
