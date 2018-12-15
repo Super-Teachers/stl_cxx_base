5dd423e1c2c302bf833ebb86a31c7d007c4c74de
Bartosz Taczała <bartosz.taczala@mobica.com>
2018-12-15 11:02:12 +0100

    cwiczenie 1

--- exercises/main.cpp
+++ exercises/main.cpp
@@ -3,10 +3,15 @@
 #include <cmath>
 #include <iterator>
 #include <list>
+#include <numeric>
 #include <vector>
 
 template <typename T>
 void print(T&& t, std::ostream& os = std::cout) {
+    if (std::begin(t) == std::end(t)) {
+        os << "[]\n";
+        return;
+    }
     std::cout << "[";
     std::for_each(std::begin(t), std::end(t) - 1,
                   [&os](auto v) { os << v << ", "; });
@@ -15,22 +20,41 @@ void print(T&& t, std::ostream& os = std::cout) {
     std::cout << "]\n";
 }
 
-bool isAllLowercase(const std::string& s) {
-    return std::all_of(s.begin(), s.end(),
-                       [](char c) { return c == ::tolower(c); });
+void read2() {
+    std::vector<float> floats;
+    std::size_t len;
+    std::cin >> len;
+    print(floats);
+
+    std::copy_n(std::istream_iterator<float>(std::cin), len,
+                std::back_inserter(floats));
+    print(floats, std::cout);
 }
 
-void remove(std::vector<std::string>& a) {
-    auto hasNumber = [](const std::string& str) {
-        return std::any_of(str.begin(), str.end(),
-                           [](char c) { return isdigit(c); });
-    };
+std::vector<float> read(std::size_t N) {
+    std::vector<float> res;
+    std::generate_n(std::back_inserter(res), N, []() {
+        float f;
+        std::cin >> f;
+        return f;
+    });
 
-    a.erase(std::remove_if(a.begin(), a.end(), hasNumber), a.end());
+    return res;
+}
+
+float calculate(const std::vector<float>& vec) {
+    if (std::is_sorted(vec.begin(), vec.end())) {
+        return std::accumulate(vec.begin(), vec.end(), 0.0f);
+    } else {
+        return std::accumulate(vec.begin(), vec.end(), 1.0f,
+                               std::multiplies<float>());
+    }
 }
 
 int main(int argc, char** argv) {
-    std::vector<std::string> v{"Ala", "bob1", "Ala2", "bob"};
-    remove(v);
-    print(v);
+    std::size_t N;
+    std::cin >> N;
+    const auto v = read(N);
+    std::cout << calculate(v) << "\n";
+    return EXIT_SUCCESS;
 }
